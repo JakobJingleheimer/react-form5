cbb2062285b74ac3549d41871282f8c8b63730bb
Jacob <3012099+jshado1@users.noreply.github.com>
2021-02-11 12:56:34 -0500

    🧹 extract composeData() into separate file

--- lib/Form.jsx
+++ lib/Form.jsx
@@ -6,69 +6,16 @@ import {
 	func,
 } from 'prop-types';
 
+import composeData, {
+	FIELD_TAGS,
+} from './composeData';
 import deepDiff from './deepDiff';
 
 import styles from './Form.module.css';
 
 
-const fieldTags = {
-	FIELDSET: 'fieldset',
-	INPUT: 'input',
-	SELECT: 'select',
-	TEXTAREA: 'textarea',
-};
-
-export const composeData = (
-	values,
-	{
-		checked,
-		dataTransfer,
-		elements,
-		files,
-		name,
-		tagName,
-		type,
-		value,
-		...attrs
-	},
-	i,
-	form,
-) => {
-	const tag = fieldTags[tagName];
-
-	if (tag && name) { // ignore anon fieldset
-		if (tag === 'fieldset') {
-			const nestedFieldCount = elements.length;
-			const nestedFields = form.splice( // move from master list (avoid double-counting)
-				i + 1,
-				nestedFieldCount,
-				...Array(nestedFieldCount).fill({ tagName: 'NESTED_FIELD' })
-			);
-			values[name] = _reduce(
-				nestedFields,
-				composeData,
-				Object.create(null),
-			);
-		} else if (tag === 'select' && attrs.multiple) {
-			values[name] = attrs.selectedOptions.map(({ value }) => value);
-		} else if (type === 'file') {
-			values[name] = files || dataTransfer;
-		} else if (type === 'checkbox') {
-			values[name] = checked;
-		} else if (type === 'number') {
-			///
-		} else if (type === 'radio') {
-			if (checked) values[name] = value;
-			else values[name] ??= null;
-		}
-		else values[name] ??= value || null;
-	}
-
-	return values;
-};
-
 export default class Form extends PureComponent {
-	static fieldTags = fieldTags;
+	static FIELD_TAGS = FIELD_TAGS;
 
 	initValues = Object.create(null);
 	ref = null;
